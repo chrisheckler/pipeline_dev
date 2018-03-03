6d7819c7c8f0fddf4c1a0294315f0cd49605966b
Chris Heckler <hecklerchris@hotmail.com>
2018-03-02 22:31:00 -0800

    Created luigi template
    The following changes:
    
    * Create a template to speed up workflow
    
    Address the following concerns
    
    * Increase efficiency

---
+++ luigi_template/template.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import luigi
+import json
+
+class Stage0(luigi.Task):
+    input_file = luigi.Parameter()
+    def requires(self):
+        return ...
+
+    def output(self):
+        return ...
+
+    def run(self):
+        return ... 
+
+class Stage1(luigi.Task):
+    input_file = luigi.Parameter()
+    def requires(self):
+        return ...
+
+    def output(self):
+        return ...
+
+    def run(self):
+        return ... 
+
+class Stage2(luigi.Task):
+    input_file = luigi.Parameter()
+    def requires(self):
+        return ...
+
+    def output(self):
+        return ...
+
+    def run(self):
+        return ... 
+
+class Stage3(luigi.Task):
+    input_file = luigi.Parameter()
+    def requires(self):
+        return ...
+
+    def output(self):
+        return ...
+
+    def run(self):
+        return ... 
+
+class Stage4(luigi.Task):
+    input_file = luigi.Parameter()
+    def requires(self):
+        return ...
+
+    def output(self):
+        return ...
+
+    def run(self):
+        return ... 
+
+if __name__=="__main__":
+    luigi.run()
+
+
