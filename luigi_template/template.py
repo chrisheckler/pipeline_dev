20c58215c90196721ec04f273de6ead3361841c4
Chris Heckler <hecklerchris@hotmail.com>
2018-03-02 22:52:53 -0800

    More comprehensive
    The following changes:
    
    * Added yet commented out other functionality
    
    Address the following concerns
    
    * Increase options for workflow

--- luigi_template/template.py
+++ luigi_template/template.py
@@ -4,58 +4,58 @@ import json
 
 class Stage0(luigi.Task):
     input_file = luigi.Parameter()
-    def requires(self):
-        return ...
+#    def requires(self):
+#        return  
 
     def output(self):
-        return ...
+        return luigi.LocalTarget(self.input_file)
 
-    def run(self):
-        return ... 
+#    def run(self):
+#        return ... 
 
 class Stage1(luigi.Task):
     input_file = luigi.Parameter()
-    def requires(self):
-        return ...
+#    def requires(self):
+#        return  
 
     def output(self):
-        return ...
+        return luigi.LocalTarget(self.input_file)
 
-    def run(self):
-        return ... 
+#    def run(self):
+#        return ... 
 
 class Stage2(luigi.Task):
     input_file = luigi.Parameter()
-    def requires(self):
-        return ...
+#    def requires(self):
+#        return  
 
     def output(self):
-        return ...
+        return luigi.LocalTarget(self.input_file)
 
-    def run(self):
-        return ... 
+#    def run(self):
+#        return ... 
 
 class Stage3(luigi.Task):
     input_file = luigi.Parameter()
-    def requires(self):
-        return ...
+#    def requires(self):
+#        return  
 
     def output(self):
-        return ...
+        return luigi.LocalTarget(self.input_file)
 
-    def run(self):
-        return ... 
+#    def run(self):
+#        return ... 
 
 class Stage4(luigi.Task):
     input_file = luigi.Parameter()
-    def requires(self):
-        return ...
+#    def requires(self):
+#        return  
 
     def output(self):
-        return ...
+        return luigi.LocalTarget(self.input_file)
 
-    def run(self):
-        return ... 
+#    def run(self):
+#        return ... 
 
 if __name__=="__main__":
     luigi.run()
