28c7d038f829de56ccee74c578761dfad0ca3cd0
Chris Heckler <hecklerchris@hotmail.com>
2018-03-02 21:39:35 -0800

    Pipeline complete
    The following changes:
    
    * The pipeline and outputs are satisfactory and to spec
    
    Address the following concerns
    
    * Complete the basic spec of this pipeline

---
+++ api_pipeline/README.md
@@ -0,0 +1,9 @@
+# api_pipeline
+I will take a json file and output a dictionary containing api_key's and count of status codes
+
+## Stage0
+A json file is read into the pipeline and passed onto Stage1
+
+## Stage1
+The json is parsed and the status code count for each api key is returned in json
+
---
+++ api_pipeline/pipeline.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import luigi
+import json
+#from pathlib import Path
+
+class InputFile(luigi.Task):
+    input_file = luigi.Parameter()
+    def output(self):
+        return luigi.LocalTarget(self.input_file)
+"""
+    def run(self):
+        with self.output().open('w') as f:
+            for line in open(self.input_file):
+                f.write(json.dumps(line))
+"""
+class StatusCodeCount(luigi.Task):
+    input_file = luigi.Parameter()
+    def requires(self):
+        return InputFile(self.input_file)
+
+    def output(self):
+        return luigi.LocalTarget('aggregate_api.json')
+
+#    def run(self):
+#       data_json = []
+#        with self.output().open('w') as fout:
+#            fout.write(self.input().open('r').read())
+
+    def run(self):
+        from pathlib import Path
+        data = {}
+        for line in self.input().open().read().strip().split("\n"):
+            j_line = json.loads(line)['_source']
+            if j_line.get('api_key', None) and j_line.get('status', None):
+                if data.get(j_line.get('api_key'), None):
+                    if data[j_line.get('api_key')]['status_codes'].get(j_line.get('status')):
+                        data[j_line.get('api_key')]['status_codes'][j_line.get('status')] += 1
+                    else:
+                        data[j_line.get('api_key')]['status_codes'].setdefault(j_line.get('status'), 1)
+                else:
+                    data.setdefault(j_line.get('api_key'), {'status_codes': {j_line.get('status'): 1}})
+
+        with self.output().open('w') as fout:
+            fout.write(json.dumps(data))
+
+if __name__=="__main__":
+    luigi.run()
+
+
